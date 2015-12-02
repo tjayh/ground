41b6ce8d46500c30ac12a52d6f62f1d17b04d875
Jesther Bas <dev.jestherbas@hotmail.com>
2015-12-02 15:39:09 +0800

    Alternative way to create the modules with empty directory

--- installation/functions/getmodules.php
+++ installation/functions/getmodules.php
@@ -17,21 +17,55 @@
 	}
 	
 	if($flag){
-			$modnames = array();
-		if ($handle = opendir($direc)) {
-			while (false !== ($entry = readdir($handle))) {
-				if ($entry != "." && $entry != "..") {
-					$modnames[] = $entry;
+		$modnames = array();
+		if ($handle = opendir("../modules")) {
+			if ($handle = opendir($direc)) {
+				while (false !== ($entry = readdir($handle))) {
+					if ($entry != "." && $entry != "..") {
+						$modnames[] = $entry;
+					}
+				}
+				closedir($handle);
+			}
+			else{
+				$flag = false;
+				if($modtype == 1)
+					$errorlist[] = "Failed to open basic modules directory.";
+				else
+					$errorlist[] = "Failed to open premium modules directory.";
+			}
+		} else {
+			$directory = Array();
+			$directory['basic'] = ['contactus', 'faq', 'newsletter', 'search', 'seomanager'];
+			$directory['premium'] = ['deal', 'downloads', 'job', 'merchantportal', 'newsandevents', 'registration', 'testimonial', 'transaction'];
+			$directory['premiumactive'] = ['deals', 'downloads', 'job', 'loyalty', 'merchantportal', 'newsandevents', 'registration', 'testimonial', 'transaction'];
+			
+			if (!opendir("../modules")) {
+				mkdir('../modules');
+				foreach($directory as $key => $value){
+					mkdir("../modules/" . $key);
+					foreach($value as $v){
+						mkdir("../modules/" . $key. "/".$v);
+					}
 				}
 			}
-			closedir($handle);
-		}
-		else{
-			$flag = false;
-			if($modtype == 1)
-				$errorlist[] = "Failed to open basic modules directory.";
-			else
-				$errorlist[] = "Failed to open premium modules directory.";
+			if ($handle = opendir("../modules")) {
+				if ($handle = opendir($direc)) {
+					while (false !== ($entry = readdir($handle))) {
+						if ($entry != "." && $entry != "..") {
+							$modnames[] = $entry;
+						}
+					}
+					closedir($handle);
+				}
+				else{
+					$flag = false;
+					if($modtype == 1)
+						$errorlist[] = "Failed to open basic modules directory.";
+					else
+						$errorlist[] = "Failed to open premium modules directory.";
+				}
+			}
 		}
 		if($flag){
 			$data['modnames'] = $modnames;
